416f4aee28971e844d793d7ecae0a2689e4b67e9
truefinderstudio <168911410+truefinderstudio@users.noreply.github.com>
2024-05-04 12:16:19 -0700

    Add files via upload

---
+++ converter.js
@@ -0,0 +1,26 @@
+// converter.js
+function unicodeToNCR(input) {
+    var output = '';
+    for (var i = 0; i < input.length; i++) {
+        output += '&#' + input.charCodeAt(i) + ';';
+    }
+    return output;
+}
+
+function NCRToUnicode(input) {
+    return input.replace(/&#(\d+);/g, function(match, numStr) {
+        return String.fromCharCode(parseInt(numStr, 10));
+    });
+}
+
+function convertText(input) {
+    // Define your find and replace arrays
+    var find = ["&#92;", "&#124;", "&#62;"
+]; // Placeholder: Fill with actual patterns
+    var replace = ["&#7789;", "&#7788;", "&#234;"]; // Placeholder: Fill with actual replacements
+    for (var i = 0; i < find.length; i++) {
+        var regex = new RegExp(find[i], "g");
+        input = input.replace(regex, replace[i]);
+    }
+    return input;
+}
